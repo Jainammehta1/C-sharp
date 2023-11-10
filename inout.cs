aedc8b82d9199c3f3b9a0f6f0974ef273f4d92c7
Jainammehta1 <129773752+Jainammehta1@users.noreply.github.com>
2023-11-10 11:52:45 +0530

    Add files via upload

---
+++ inout.cs
@@ -0,0 +1,24 @@
+﻿class read
+{
+    public void getdata()
+    {
+        String loc = @"D:\Jainam\jj.txt";
+        if (File.Exists(loc))
+        {
+            Console.WriteLine("File Exists");
+            String line;
+            line = File.ReadAllText(loc);
+            Console.WriteLine(line);
+        }
+        Console.ReadKey();
+    }
+
+    public void writedat(String value)
+    {
+
+        File.WriteAllText(@"D:\Jainam\jj.txt", value);
+
+
+    }
+
+}
