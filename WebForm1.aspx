22d43fdb358d0897742ecd009ada7dc66e0f2a26
Jainammehta1 <129773752+Jainammehta1@users.noreply.github.com>
2024-01-28 21:31:27 +0530

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.signin.WebForm1" %>
+
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Login</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="UserNameLabel" runat="server" Text="Username:"></asp:Label>
+            <asp:TextBox ID="UserNameTextBox" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="PasswordLabel" runat="server" Text="Password:"></asp:Label>
+            <asp:TextBox ID="PasswordTextBox" runat="server" TextMode="Password"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Button ID="LoginButton" runat="server" Text="Login" OnClick="LoginButton_Click" />
+        </div>
+        <div>
+            <asp:Label ID="ErrorLabel" runat="server" ForeColor="Red"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
+
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.signin
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            string userName = UserNameTextBox.Text.Trim();
+            string password = PasswordTextBox.Text.Trim();
+
+            // Replace this with your own logic to validate user credentials
+            if (userName == "admin" && password == "password")
+            {
+                // Redirect to a new page if the user has the right credentials
+                Response.Redirect("~/About.aspx");
+            }
+            else
+            {
+                // Display an error message if the user has the wrong credentials
+                ErrorLabel.Text = "Invalid username or password.";
+            }
+        }
+    }
+}
